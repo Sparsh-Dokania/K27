1e09055acdbf2d644de46d390dd1c3f02bad5bd6
Sparsh-Dokania <sparshdokania2004@gmail.com>
2025-11-29 01:25:50 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className='h-screen w-screen bg-red-700 '>
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
